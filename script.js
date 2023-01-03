73bb7c099d69c5f8258f5a4fbe089274c4c7ceb6
Nugroho AR <mit.shuki23@gmail.com>
2023-01-04 01:17:53 +0700

    tumbal

---
+++ script.js
@@ -0,0 +1,12 @@
+function coba(R) {
+    var myElement = document.getElementById(R);
+    if (myElement) myElement.style.display = "none";
+    showLink.style.display = "block";
+    hideLink.style.display = "none";
+}
+function cobaLagi(R) {
+    var myElement = document.getElementById(R);
+    if (myElement) myElement.style.display = "block";
+    showLink.style.display = "none";
+    hideLink.style.display = "block";
+}
