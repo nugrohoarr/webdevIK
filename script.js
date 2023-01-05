2da500b71666fb38580dc76f405b186e1be70939
Nugroho AR <mit.shuki23@gmail.com>
2023-01-05 23:42:51 +0700

    lelah bang

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-const panels = document.querySelectorAll('.selected');
+// Collapsibles
+let coll = document.getElementsByClassName("collapsible");
 
-function toggleOpen() {
-  this.classList.toggle('open');
+for (let i = 0; i < coll.length; i++) {
+  // ketika class collapsible di click, jalankan:
+  coll[i].addEventListener("click", function() {
+    removeCollapseClass();
+    // tambahkan class active didalam tag class collapsible
+    this.classList.toggle("active");
+
+    // tambahkan style display didalam tag element selanjutnya
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
 
-function toggleOpenActive(e) {
-  console.log(e.propertyName); 
-
-  if (e.propertyName.includes('flex')) {
-    this.classList.toggle('selected');
+// fungsi untuk menghilangkan collapse sebelumnya
+function removeCollapseClass() {
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].classList.remove("active");
+    coll[i].nextElementSibling.style.display = "none";
   }
 }
-
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleOpenActive));
